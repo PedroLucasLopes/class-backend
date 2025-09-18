b5c5345a788703d045574891ca3bb058c765d1e7
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-09-18 19:06:38 -0300

    fix/CF002

--- main.ts
+++ main.ts
@@ -5,10 +5,16 @@ import { StudentsService } from "./src/module/core/students/service/students.ser
 const app = express();
 const port = 3000;
 
+// Student Module Setup
 const studentsService = new StudentsService();
 const studentsController = new StudentsController(studentsService);
 
+// Student Routes
 app.get("/users", (req, res) => studentsController.findAll(req, res));
+app.get("/users/:id", (req, res) => studentsController.findById(req, res));
+app.post("/users", (req, res) => studentsController.create(req, res));
+app.put("/users/:id", (req, res) => studentsController.update(req, res));
+app.delete("/users/:id", (req, res) => studentsController.delete(req, res));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- src/module/core/students/controller/students.controller.ts
+++ src/module/core/students/controller/students.controller.ts
@@ -15,4 +15,56 @@ export class StudentsController {
       res.status(500).json({ message: "Internal server error" });
     }
   }
+
+  // GET /users/:id
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const user = await this.studentsService.findById(id);
+      if (user) {
+        return res.json(user);
+      }
+      return res.status(404).json({ message: "User not found" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  // POST /users
+  async create(req: Request, res: Response) {
+    const data = req.body;
+    try {
+      const newUser = await this.studentsService.create(data);
+      return res.status(201).json(newUser);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  // PUT /users/:id
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const data = req.body;
+    try {
+      const updatedUser = await this.studentsService.update(id, data);
+      return res.json(updatedUser);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  // DELETE /users/:id
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      await this.studentsService.delete(id);
+      return res.status(204).send();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
 }
