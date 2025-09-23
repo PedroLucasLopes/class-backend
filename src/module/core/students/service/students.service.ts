890e19d440eb7d61aacff90b0c675132b39d810e
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-09-23 14:26:10 -0300

    CF011

--- src/module/core/students/service/students.service.ts
+++ src/module/core/students/service/students.service.ts
@@ -5,16 +5,33 @@ import { IStudent } from "../model/poststudent.model";
 import { IUpdateStudent } from "../model/updatestudent.model";
 import { orderEnum } from "../../../../shared/model/pagination.model";
 import { normalizeEmail } from "../../../../shared/utils/normalize-email.utils";
+import { Prisma } from "@prisma/client";
+import { logger } from "../../../../shared/log/_logger";
 
 export class StudentsService {
-  async findAll(page?: number, limit?: number, orderBy?: orderEnum) {
+  async findAll(
+    page?: number,
+    limit?: number,
+    orderBy?: orderEnum,
+    search?: string
+  ) {
+    const where: Prisma.StudentWhereInput = search
+      ? {
+          OR: [
+            { name: { contains: search, mode: "insensitive" } },
+            { email: { contains: search, mode: "insensitive" } },
+            { ra: { contains: search, mode: "insensitive" } },
+            { cpf: { contains: search, mode: "insensitive" } },
+          ],
+        }
+      : {};
     const paginationRegister = Number(limit) || 10;
-    const paginationInterval =
-      ((Number(page) || 1) - 1) * (Number(limit) || 10);
+    const paginationInterval = (Number(page) || 1 - 1) * (Number(limit) || 10);
     return prisma.student.findMany({
       take: paginationRegister,
       skip: paginationInterval,
       orderBy: { createdAt: orderBy || "desc" },
+      where,
     });
   }
 
